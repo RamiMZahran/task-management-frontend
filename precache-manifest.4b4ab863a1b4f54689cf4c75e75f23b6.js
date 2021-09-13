d7ea563763f69d715f21ae52f747c478ed4f7f7d
RamiMZahran <RamiMZahran@users.noreply.github.com>
2021-09-13 08:22:04 +0000

    deploy: 8b42dd38cdb833f5dd69a2ee70936695352c13e7

---
+++ precache-manifest.4b4ab863a1b4f54689cf4c75e75f23b6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3cc6ce63c9c63eaab64a9d261cce17c",
+    "url": "https://ramimzahran.github.io/task-management-frontend/index.html"
+  },
+  {
+    "revision": "e956287fb260d53bc9be",
+    "url": "https://ramimzahran.github.io/task-management-frontend/static/css/main.5ac26d30.chunk.css"
+  },
+  {
+    "revision": "1d336943785b89b6269f",
+    "url": "https://ramimzahran.github.io/task-management-frontend/static/js/2.a64ce860.chunk.js"
+  },
+  {
+    "revision": "e956287fb260d53bc9be",
+    "url": "https://ramimzahran.github.io/task-management-frontend/static/js/main.38ee8424.chunk.js"
+  },
+  {
+    "revision": "4d4a242eeab3e0818bf9",
+    "url": "https://ramimzahran.github.io/task-management-frontend/static/js/runtime~main.a79b7eec.js"
+  }
+]);
